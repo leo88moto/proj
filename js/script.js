1161105211d2307be0ea84d351dd0b7fbe2cb657
leo <leo88moto@gmail.com>
2024-01-16 21:06:39 +0300

    second

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 "use strict";
 
-const answers = [];
 
-console.log(typeof(answers));
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const film = prompt('Последний фильм?', '');
+const rate = prompt('Оцените в формате 8.0', '');
+const film2 = prompt('Последний фильм?', '');
+const rate2 = prompt('Оцените в формате 8.0', '');
+
+personalMovieDB.movies[film] = rate;
+personalMovieDB.movies[film2] = rate2;
+
+console.log(personalMovieDB);
